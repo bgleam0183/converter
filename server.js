0a60fc8cc4381f766cab822c51349f8c59008171
tjddnr9 <tjddnr9@jointree.co.kr>
2022-05-25 17:41:51 +0900

    same as before comment with A.js

--- server.js
+++ server.js
@@ -24,13 +24,12 @@ async function oraSelect(req, res) {
         var result = await connection.execute(`SELECT * FROM TB_TEST WHERE 1=1`, []);
 
         console.log("\n\n################# express Execute Value #################\n");
-        console.log(result);
+        console.log(result.rows);
         console.log("\n\n############## express Value Transfer Ended ##############\n")
 
-        res.setHeader("Content-Type", "application/json");
-        res.json(result);
-        // res.send(result);
-        res.end();
+        console.log(`express received Data : ${req.body.input}`);
+
+        res.send(result.rows[0]);
     } catch (err) {
         console.log(err);
     } finally {
@@ -43,7 +42,7 @@ async function oraSelect(req, res) {
 }
 
 
-app.get("/asd", (req, res) => {
+app.post("/asd", (req, res) => {
     oraSelect(req, res);
 });
 
